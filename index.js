09943bbc04dce32119e19638ab66f0d78cf41b1c
ianstrom <iankstrom@gmail.com>
2022-11-11 11:57:35 -0800

    commit tree da025acf75a49c5aa6146917f94ffec7c70b75ef

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const element = document.getElementById('main');
+element.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Ian is the champion'
+document.body.append(newHeader);
